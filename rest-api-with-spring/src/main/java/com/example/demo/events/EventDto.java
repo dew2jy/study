528d9b32ec56e9de054298454a2866cb4454701f
LeeJi-Yeon <jylee@LeeJi-Yeonui-MacBook-Pro.local>
2020-01-30 17:04:11 +0900

    DTO validation

--- rest-api-with-spring/src/main/java/com/example/demo/events/EventDto.java
+++ rest-api-with-spring/src/main/java/com/example/demo/events/EventDto.java
@@ -6,20 +6,33 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Builder @NoArgsConstructor @AllArgsConstructor
 @Data
 public class EventDto {
     //Event 엔티티에 애노테이션 존많문... 그래서 입력값만 분리...
+    @NotEmpty
     private String name;
+    @NotEmpty
     private String description;
+    @NotNull
     private LocalDateTime beginEnrollmentDateTime;
+    @NotNull
     private LocalDateTime closeEnrollmentDateTime;
+    @NotNull
     private LocalDateTime beginEventDateTime;
+    @NotNull
     private LocalDateTime endEventDateTime;
+    @NotNull
     private String location; // (optional) 이게 없으면 온라인 모임
+    @Min(0)
     private int basePrice; // (optional)
+    @Min(0)
     private int maxPrice; // (optional)
+    @Min(0)
     private int limitOfEnrollment;
 }
