2d55c667e1ed01b9dc14beb8ae5d73b1067aa574
LeeJi-Yeon <jylee@LeeJi-Yeonui-MacBook-Pro.local>
2020-01-30 09:48:55 +0900

    initial settings

--- rest-api-with-spring/src/main/java/me/whiteship/demoinfleanrestapi/accounts/AccountRole.java
+++
@@ -1,6 +0,0 @@
-package me.whiteship.demoinfleanrestapi.accounts;
-
-public enum AccountRole {
-
-    ADMIN, USER
-}
--- rest-api-with-spring/src/main/java/me/whiteship/demoinfleanrestapi/events/EventStatus.java
+++
@@ -1,7 +0,0 @@
-package me.whiteship.demoinfleanrestapi.events;
-
-public enum EventStatus {
-
-    DRAFT, PUBLISHED, BEGAN_ENROLLMENT;
-
-}
