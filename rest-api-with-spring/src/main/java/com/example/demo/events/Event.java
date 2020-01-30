5a50390fe145eec29c0543e8181b71624ef91d4f
LeeJi-Yeon <jylee@LeeJi-Yeonui-MacBook-Pro.local>
2020-01-30 20:31:06 +0900

    Business logic tests

--- rest-api-with-spring/src/main/java/com/example/demo/events/Event.java
+++ rest-api-with-spring/src/main/java/com/example/demo/events/Event.java
@@ -28,4 +28,20 @@ public class Event {
     @Enumerated(EnumType.STRING)
     private EventStatus eventStatus = EventStatus.DRAFT;
 
+    public void update() {
+        //update free
+        if(this.basePrice == 0 && this.maxPrice == 0) {
+            this.free = true;
+        } else {
+            this.free = false;
+        }
+
+        //update offline
+        //java 11 => isBlank()
+        if(this.location == null || this.location.trim().isEmpty()) {
+            this.offline = false;
+        } else {
+            this.offline = true;
+        }
+    }
 }
